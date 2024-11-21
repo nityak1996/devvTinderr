d4ad56cb8b8e6a19fb5b4d5ce1626270578d6e32
nityak1996 <nityak1996@gmail.com>
2024-11-21 18:13:35 +0530

    api call make user instance

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  //  Creating a new  instance of the User model
+  const user = new User({
+    firstName:"Loko",
+    lastName: "Singh",
+    emailId: "lokok1996@gmail.com",
+    password: "loko@123",
+  });
+  await user.save();
+  res.send("User Added Successfully")  
+});
 
 connectDB()
   .then(() => {
