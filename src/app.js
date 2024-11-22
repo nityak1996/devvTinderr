0699c778524201093d606f44f8c000076a06742b
nityak1996 <nityak1996@gmail.com>
2024-11-22 09:41:04 +0530

    CRUD Operation

--- src/app.js
+++ src/app.js
@@ -17,6 +17,62 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log(userEmail);
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API - GET /feed- get all the users from the databse..
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+// delete the user from the database...
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // both way to find id for deleting..
+    const user = await User.findByIdAndDelete({ _id: userId });
+    // OR
+    // const user = await User.findByIdAndDelete(userId);
+    res.send("user Deleted Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update data fo the user...
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log(data);
+  try {
+   const user =  await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    // by default value "before " alaways...
+    console.log(user)
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection is established...");
