e2f69c66e26bc97defc0e24ded87cc068f6996a9
nityak1996 <nityak1996@gmail.com>
2024-11-23 09:17:44 +0530

    data_validation .

--- src/app.js
+++ src/app.js
@@ -69,10 +69,10 @@ app.patch("/user/:userId", async (req, res) => {
     if (!isUpdateAllowed) {
       throw new Error("Update not allowed");
     }
-
-    if (data.skills.length > 5) {
-      throw new Error("Skills can't be more than 10");
-    }
+  // this is api based validation
+    // if (data.skills.length > 5) {
+    //   throw new Error("Skills can't be more than 10");
+    // }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address: " + value);
+        }
+      },
     },
     password: {
       type: String,
       required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password : " + value);
+        }
+      },
     },
     age: {
       type: Number,
@@ -37,15 +48,26 @@ const userSchema = new mongoose.Schema(
     photoUrl: {
       type: String,
       default: "https://avatars.githubusercontent.com/u/7790161?v=4",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url address: " + value);
+        }
+      },
     },
     about: {
       type: String,
       default: "This is a default about of the user!!!",
     },
+    // this is schema based validation !!!
     skills: {
       type: [String],
-      
-    
+      validate: {
+        validator: function (value) {
+          if (value.length > 5) {
+            throw new Error("You can't have more than 5 skills");
+          }
+        },
+      },
     },
   },
   { timestamps: true }
